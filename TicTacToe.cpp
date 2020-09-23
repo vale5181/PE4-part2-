a71cffd8ed4f3d92ebe9859a809ca1c663fedd96
ishakarki <iska6979@colorado.edu>
2020-09-23 13:03:54 -0600

    ishas displayboard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -11,17 +11,21 @@ void CreateBoard(int ar[3][3])
     }
 }
 
+void DisplayBoard(int ar[3][3])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout << " " << ar[i][j];
+        }
+        std::cout << "\n";
+    }
+}
+
 int main()
 {
     int ar[3][3];
     CreateBoard(ar);
-    // printing board
-    // for(int i = 0; i < 3; i++)
-    // {
-    //     for (int j = 0; j < 3; j++)
-    //     {
-    //         std::cout << ar[i][j];
-    //     }
-    //     std::cout << "\n";
-    // }
+    DisplayBoard(ar);
 }
